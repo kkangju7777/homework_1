2be2203746f62d76820cbc7ee13743a8fbb116a9
kkangju7777 <kkangju7777@naver.com>
2020-10-17 16:10:03 +0900

    modify file

--- cnt_seq.py
+++ cnt_seq.py
@@ -1,13 +1,21 @@
 #! /usr/bin/python3
-import sys
-
+a,t,c,g = 0,0,0,0
 cnt = 0
 
-with open(sys.argv[1]) as handle:
+with open('sam_seq.txt') as handle:
     for line in handle:
         line = line.strip()
         if line.startswith('>'):
             continue
-        cnt += len(line)
-print(cnt)
+        a += line.count('A')
+        t += line.count('T')
+        c += line.count('C')
+        g += line.count('G')
+    
+print("A :",a)
+print("C :",c)
+print("T :",t)
+print("G :",g)
+
+
 
